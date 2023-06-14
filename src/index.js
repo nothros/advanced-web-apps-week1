3a445c07944dc7dc401f914ebb61129b240f652f
Erika Horros-Wallin <erika.horros@gmail.com>
2023-06-14 12:43:26 +0300

    Smallfix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import "./styles.css";
 
-if (document.readyState !== "loading") {
-  console.log("DOCUMENT READY");
-  initializeCode();
-} else {
-  document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOCUMENT NOT READY");
-    initializeCode();
-  });
-}
-
-
 const fetchData = async (url) => {
   const response = await fetch(url);
   const json = await response.json();
@@ -21,15 +10,15 @@ const fetchData = async (url) => {
 const makeElement = (element, className, content) => {
   const webElement = document.createElement(element);
   webElement.className = className;
-  if(element === "img"){
+  if (element === "img") {
     webElement.src = content;
   }
-  if(element === "p" || element === "h1"){
-    webElement.textContent = content
+  if (element === "p" || element === "h1") {
+    webElement.textContent = content;
   }
 
   return webElement;
-}
+};
 
 const createWikiItem = (picture, breed, info) => {
   const wikiItem = makeElement("div", "wiki-item");
@@ -39,27 +28,24 @@ const createWikiItem = (picture, breed, info) => {
   const wikiContent = makeElement("div", "wiki-content");
   const wikiText = makeElement("p", "wiki-text", info);
 
-  wikiContent.appendChild(wikiText);
-
   const imgContainer = makeElement("div", "img-container");
 
-  const wikiImg= makeElement("img", "wiki-img", picture);
-  
+  const wikiImg = makeElement("img", "wiki-img", picture);
+
+  wikiItem.appendChild(wikiContent);
   imgContainer.appendChild(wikiImg);
   wikiContent.appendChild(imgContainer);
-  wikiItem.appendChild(wikiContent);
-
+  wikiContent.appendChild(wikiText);
 
   return wikiItem;
-}
+};
 
-const createContent = async () =>  {
-  const breeds = ["Husky", "Kelpie", "Beagle", "Borzoi", "Malamute"]
+const createContent = async () => {
+  const breeds = ["Husky", "Kelpie", "Beagle", "Borzoi", "Malamute"];
   const container = makeElement("div", "container");
   document.body.appendChild(container);
 
   for (let index = 0; index < 5; index++) {
-    
     const breed = breeds[index];
 
     const wikiURL = `https://en.wikipedia.org/api/rest_v1/page/summary/${breed}?redirect=true"`;
@@ -67,11 +53,20 @@ const createContent = async () =>  {
 
     const info = await fetchData(wikiURL);
     const picture = await fetchData(picURL);
-    
+
     container.appendChild(createWikiItem(picture.message, breed, info.extract));
   }
-
-}
+};
 const initializeCode = async () => {
   createContent();
 };
+
+if (document.readyState !== "loading") {
+  console.log("DOCUMENT READY");
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOCUMENT NOT READY");
+    initializeCode();
+  });
+}
